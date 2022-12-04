0fffc51ce3de57f6b778f47b9312f4e6b20ce965
Theawesometurtl <shablammmo@gmail.com>
2022-12-04 13:11:07 -0800

    I added some comments so I'm not lost when I inevitably get lost in this mess

--- src/actions/momentOfInertia.js
+++ src/actions/momentOfInertia.js
@@ -1,3 +1,37 @@
+
+/*
+What I am doing here is splitting up the polygon into many seperate triangles and
+finding the moment of inertia for each triangle individually, then using the parrellel
+axis theorum to move each triangle's moment of inertia to the centroid of the polygon.
+
+For each triangle, I convert it's base into a line using the formula y = mx + b.
+Then, I find the slope of the line in order to make a parallel infinite line on
+the point at the tippy top of the triangle. Measuring the bias between the two lines
+is equivalent to measuring the height of the triangle perpandicular to its base.
+
+I love that I am using basic geometry and algebra to find the base and height of the
+triangle, and proud I came up with the method on my own. It works, but I'm not sure
+if the formula I applied to get the moment of inertia of the triangle is correct.
+
+I think I want a formula that gives the moment of inertia for a triangle with an axis
+perpandicular to the plane of the triangle at the centroid. I am almost certain that
+the formula I am using right now doesn't do what I want it to do.
+
+from there, I use the parellel axis theorum to move the centroid of the triangle to 
+the center of the polygon.
+
+I am pretty happy with all I have right now, I still need to find the right formula
+for the moment of inertia, and I also need to spread the mass of the polygon evenly,
+probably by finding the area of each triangle and distributing the mass of the polygon
+accordingly.
+
+If I want to clean up the code I have and continue to work on it, I should probably
+change the functions so the arguments that are passed in are passed in as an
+arrangement of lists inside a dictionary for x + y coordinates. idk tho.
+maybe they should be passed as lists with a dictionary of x + y coordinates instead.
+
+
+*/
 function momentOfInertia(vertices, centroid) {
     if (vertices.x.length < 3) {return null;};
     let inertia = 0;
